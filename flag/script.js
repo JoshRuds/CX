d12f9715515ceb557bdd7a7fb51fd376c9d33b71
JoshRuds <joshua.ruddick@icloud.com>
2025-10-07 17:46:43 +0100

    Update script.js

--- flag/script.js
+++ flag/script.js
@@ -16,13 +16,21 @@ async function setup() {
   var flag4 = document.getElementById("flag4");
   var flagimg = document.getElementById("flagimg");
 
-  flag1.innerHTML = await randomPng();
-  flag2.innerHTML = await randomPng();
-  flag3.innerHTML = await randomPng();
-  flag4.innerHTML = await randomPng();
-  var flags = [flag1,flag2,flag3,flag4];
-  var flag = flags[Math.floor(Math.random() * 4)].innerHTML;
-  
+  // get four random image paths
+  var f1 = await randomPng();
+  var f2 = await randomPng();
+  var f3 = await randomPng();
+  var f4 = await randomPng();
+
+  // show just filenames as button text (not full paths)
+  flag1.innerHTML = f1.split("/").pop().replace(".png", "");
+  flag2.innerHTML = f2.split("/").pop().replace(".png", "");
+  flag3.innerHTML = f3.split("/").pop().replace(".png", "");
+  flag4.innerHTML = f4.split("/").pop().replace(".png", "");
+
+  // pick one randomly for the displayed flag
+  var flags = [f1, f2, f3, f4];
+  var flag = flags[Math.floor(Math.random() * 4)];
 
   flagimg.setAttribute("src", flag);
 };
